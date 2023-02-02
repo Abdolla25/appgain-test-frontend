a2c4224fc47de466ca2853b91de0e867fecea5fa
Abdolla25 <abdollaz225@gmail.com>
2023-02-02 14:18:27 +0200

    Added a Procfile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from 'react';
-import { Button, Form, InputGroup } from "react-bootstrap";
+import { Button, Form, InputGroup, Table } from "react-bootstrap";
 import './App.css';
 
 const App = () => {
   const [links, setLinks] = useState <any[]> ([])
+  const [slug, setSlug] = useState('');
+  const [ios_primary, setIosPrimary] = useState('');
+  const [ios_fallback, setIosFallback] = useState('');
+  const [android_primary, setAndroidPrimary] = useState('');
+  const [android_fallback, setAndroidFallback] = useState('');
+  const [web, setWeb] = useState('');
 
   useEffect(() => {
     fetch('https://quiet-hollows-04464.herokuapp.com/api/shortlinks', {
-      mode: 'no-cors',
       method: 'GET' 
     })
       .then(res => {
-        console.log(res)
         return res.json()
       })
       .then((data) => {
-        console.log(data);
-        setLinks(data);
+        setLinks(data.shortlinks);
       })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
+
+  const handleSubmit = () => {
+    fetch('https://quiet-hollows-04464.herokuapp.com/api/shortlinks', {
+       method: 'POST',
+       body: JSON.stringify({
+        slug: slug,
+        ios: {
+          primary: ios_primary,
+          fallback: ios_fallback
+        },
+        android: {
+          primary: android_primary,
+          fallback: android_fallback
+        },
+        web: web,
+       }),
+       headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+       },
+    })
+       .then((res) => res.json())
+       .catch((err) => {
+          console.log(err.message);
+       });
+  };
 
   return (
     <div className="App">
@@ -36,43 +64,43 @@ const App = () => {
             <h2>
               Create Shortlink
             </h2>
-            <Form>
-              <InputGroup className="mb-3">
+            <Form onSubmit={handleSubmit}>
+              <InputGroup size="sm" className="mb-3">
                 <InputGroup.Text>Slug</InputGroup.Text>
-                <Form.Control aria-label="Slug" />
+                <Form.Control aria-label="Slug" onChange={(e) => setSlug(e.target.value)} />
               </InputGroup>
 
               <hr />
 
-              <InputGroup className="mb-3">
+              <InputGroup size="sm" className="mb-3">
                 <InputGroup.Text>IOS</InputGroup.Text>
-                <Form.Control aria-label="IOS Primary" />
+                <Form.Control aria-label="IOS Primary" onChange={(e) => setIosPrimary(e.target.value)} />
                 <InputGroup.Text>Primary</InputGroup.Text>
               </InputGroup>
-              <InputGroup className="mb-3">
+              <InputGroup size="sm" className="mb-3">
                 <InputGroup.Text>IOS</InputGroup.Text>
-                <Form.Control aria-label="IOS Fallback" />
+                <Form.Control aria-label="IOS Fallback" onChange={(e) => setIosFallback(e.target.value)} />
                 <InputGroup.Text>Fallback</InputGroup.Text>
               </InputGroup>
 
               <hr />
 
-              <InputGroup className="mb-3">
+              <InputGroup size="sm" className="mb-3">
                 <InputGroup.Text>Android</InputGroup.Text>
-                <Form.Control aria-label="Android Primary" />
+                <Form.Control aria-label="Android Primary" onChange={(e) => setAndroidPrimary(e.target.value)} />
                 <InputGroup.Text>Primary</InputGroup.Text>
               </InputGroup>
-              <InputGroup className="mb-3">
+              <InputGroup size="sm" className="mb-3">
                 <InputGroup.Text>Android</InputGroup.Text>
-                <Form.Control aria-label="Android Fallback" />
+                <Form.Control aria-label="Android Fallback" onChange={(e) => setAndroidFallback(e.target.value)} />
                 <InputGroup.Text>Fallback</InputGroup.Text>
               </InputGroup>
 
               <hr />
 
-              <InputGroup className="mb-3">
+              <InputGroup size="sm" className="mb-3">
                 <InputGroup.Text>Web</InputGroup.Text>
-                <Form.Control aria-label="Web" />
+                <Form.Control aria-label="Web" onChange={(e) => setWeb(e.target.value)} />
               </InputGroup>
 
               <Button variant="secondary" type="submit">
@@ -81,22 +109,41 @@ const App = () => {
               {/* <div className="message">{message ? <p>{message}</p> : null}</div> */}
             </Form>
         </div>
-        <div>
+        <div className="widthContain">
             <h2>
               List All Shortlinks
             </h2>
             <div>
-            {links.map((link) => {
-               return (
-                  <div className="post-card" key={link.slug}>
-                     <h2 className="post-title">{link.ios.primary}</h2>
-                     <p className="post-body">{link.ios.fallback}</p>
-                     <h2 className="post-title">{link.android.primary}</h2>
-                     <p className="post-body">{link.android.fallback}</p>
-                     <h2 className="post-title">{link.web}</h2>
-                  </div>
-               );
-            })}
+              <Table striped bordered hover size="sm">
+                <thead>
+                  <tr>
+                    <th rowSpan={2}>Slug</th>
+                    <th colSpan={2}>IOS</th>
+                    <th colSpan={2}>Android</th>
+                    <th rowSpan={2}>Web</th>
+                  </tr>
+                  <tr>
+                    <td>Primary</td>
+                    <td>Fallback</td>
+                    <td>Primary</td>
+                    <td>Fallback</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {links.map((link) => {
+                    return (
+                        <tr key={link.slug}>
+                          <td>{link.slug}</td>
+                          <td>{link.ios.primary}</td>
+                          <td>{link.ios.fallback}</td>
+                          <td>{link.android.primary}</td>
+                          <td>{link.android.fallback}</td>
+                          <td>{link.web}</td>
+                        </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
             </div>
         </div>
       </main>
